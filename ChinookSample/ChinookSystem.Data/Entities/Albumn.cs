58b7609ee2f1f8ec49ab4858639ce10d03e29177
jwang87 <jwang77@studentmail.nait.ca>
2016-09-26 16:15:53 -0600

    Enities Add Artist and Albumn

---
+++ ChinookSample/ChinookSystem.Data/Entities/Albumn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region AdditionalSpaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("Albumns")]
+    class Albumn
+    {
+        [Key]
+        public int AlumnId { get; set; }
+        public string Title { get; set; }
+        public int ArtistId { get; set; }
+        public int ReleaseYear { get; set; }
+        public string ReleaseLabel { get; set; }
+    }
+}
---
+++ ChinookSample/ChinookSystem.Data/Entities/Artist.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region AdditionalSpaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("Artists")]
+    public class Artist
+    {
+        //Key notation is optional if the sql pkey
+        //ends in ID or Id
+        //required if default of entity is NOT Identity
+        //required if pkey is compound
+        
+        //Properties can be full implemented or auto implemented
+        //Properties name should use sql attributes name
+        //Properties should be listed in the same order as sql table attributes for ease of maintenance      
+        [Key]
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+}
