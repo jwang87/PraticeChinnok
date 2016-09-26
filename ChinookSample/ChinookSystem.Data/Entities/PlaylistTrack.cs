4ac3e3713219b0fe2ae4feb368bd99bc7f9f6952
jwang87 <jwang77@studentmail.nait.ca>
2016-09-26 17:07:11 -0600

    Add PlaylistTrack Class

--- ChinookSample/ChinookSystem.Data/Entities/PlaylistTrack.cs
+++ ChinookSample/ChinookSystem.Data/Entities/PlaylistTrack.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region AdditionalSpaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
 namespace ChinookSystem.Data.Entities
 {
+    [Table("PlaylistTracks")]
     public class PlaylistTrack
     {
+        [Key]
+        public int PlaylistId { get; set; }
+        public int TrackId { get; set; }
+
+        public virtual ICollection<Track> Tracks { get; set; }
     }
 }
