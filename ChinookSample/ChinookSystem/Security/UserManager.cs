dfda73a9876d86c62d8330c38ec13db3e339838c
jwang87 <jwang77@studentmail.nait.ca>
2016-10-13 16:14:01 -0600

    Move Class to componet lib
    move classes to security folder ApplicationUser, ApplicationDbContext,
    UserManager classes

---
+++ ChinookSample/ChinookSystem/Security/UserManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.AspNet.Identity.EntityFramework; //UserStore
+using Microsoft.AspNet.Identity;
+#endregion
+
+
+namespace ChinookSystem.Security
+{
+    public class UserManager : UserManager<ApplicationUser>
+    {
+        public UserManager()
+            : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
+        {
+        }
+    }
+}
