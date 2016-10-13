9140a176d5651ef69969b8792487d86e4b8a448f
jwang87 <jwang77@studentmail.nait.ca>
2016-10-13 16:41:00 -0600

    Add additional tracking properties for ApplicationUser
    Fields can act as foriegn value into the user data

--- ChinookSample/ChinookSystem/Security/ApplicationUser.cs
+++ ChinookSample/ChinookSystem/Security/ApplicationUser.cs
@@ -10,9 +10,13 @@ using Microsoft.AspNet.Identity.EntityFramework; //IdentityUser
 
 namespace ChinookSystem.Security
 {
-
+    // You can add User data for the user by adding more properties 
+    //to your User class, 
+    //please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public int? EmployeeId { get; set; }
+        public int? CustomerId { get; set; }
     }
 
 }
