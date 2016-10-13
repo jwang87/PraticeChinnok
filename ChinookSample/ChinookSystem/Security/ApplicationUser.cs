9e33af6793b6ea828dad01d218a4c65a06eece73
jwang87 <jwang77@studentmail.nait.ca>
2016-10-13 16:20:57 -0600

    Create Role manager class

--- ChinookSample/ChinookSystem/Security/ApplicationUser.cs
+++ ChinookSample/ChinookSystem/Security/ApplicationUser.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+#region Additional Namespace
 using Microsoft.AspNet.Identity.EntityFramework; //IdentityUser
+#endregion
 
 namespace ChinookSystem.Security
 {
